e5b85bf85dab8bff9131dd66d4907f4bb0cf2baf
Watersfall <dontshoot262@yahoo.com>
2017-07-26 15:18:34 -0400

    Add frame that asks the user to enter the ID

---
+++ src/web/diplomacy/turn/viewer/IdGetter.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package web.diplomacy.turn.viewer;
+
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+
+/**
+ *
+ * @author Christopher
+ */
+public class IdGetter {
+    
+    private int id = -3488975;
+    private JFrame panel;
+    private JTextField idField;
+    private JButton button;
+    private ViewingFrame frame;
+    private TurnImages array;
+    
+    public IdGetter()
+    {
+        panel = new JFrame("Enter the game ID");
+        panel.setLayout(new FlowLayout());
+        panel.setAlwaysOnTop(true);
+        
+        idField = new JTextField(20);
+        idField.setToolTipText("Enter the game ID");
+        panel.add(idField);
+        
+        button = new JButton("Go!");
+        button.addActionListener(new ActionListener() 
+        {
+            @Override
+            public void actionPerformed(ActionEvent e) 
+            {
+                id = Integer.parseInt(idField.getText());
+            }
+        });
+        panel.add(button);
+        
+        panel.pack();
+        panel.setVisible(true);
+    }
+    
+    public int getId()
+    {
+        return id;
+    }
+    
+    public void remove()
+    {
+        panel.setVisible(false);
+    }
+}
